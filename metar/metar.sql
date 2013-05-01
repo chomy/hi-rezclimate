f5f5ed5943d4122e323d149a177173638db81032
Keisuke Nakao <chome@argv.org>
2013-05-01 20:19:20 +0900

    add table report
    Signed-off-by: Keisuke Nakao <chome@planet.k.nakao.name>

--- metar/metar.sql
+++ metar/metar.sql
@@ -6,4 +6,16 @@ lng	float,
 elev	int
 );
 
+CREATE TABLE report (
+icao	char(4) not null,
+time	timestamp, 
+wind_dir	int,
+wind_kts	int,
+temp	int,
+dew	int,
+bar	int,
+raw	varchar(64)
+);
+
 CREATE INDEX idx_stations_icao ON station (icao);
+CREATE INDEX idx_report ON report(icao, time);
